4894dac2e6b265524881642c8202446b08af0092
MatheusGrigio14 <mgsilva14@stefanini.com>
2024-11-20 21:47:58 -0300

    feat(): save progress.

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,24 @@
+// Theme toggle
+const themteToggleBtn = document.querySelector('#theme-toggle');
+const theme = localStorage.getItem('theme');
+
+theme && document.body.classList.add(theme);
+
+themteToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    if (document.body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+//Hambuger menu
+const hamburguer = document.querySelector('#hamburguer');
+const nav = document.querySelector('nav');
+
+hamburguer.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamburguer.classList.toggle('open');
+});
