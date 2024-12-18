a3f0670ab2011826a765b1ed6bd01d06c0ea1d66
MatheusGrigio14 <mgsilva14@stefanini.com>
2024-12-18 02:50:42 -0300

    feat(): week 06.

--- site/scripts/products.js
+++ site/scripts/products.js
@@ -1,6 +1,10 @@
 export async function getProducts() {
+  try {
     const productsResponse = await fetch("data/products.json");
     const products = await productsResponse.json();
-  
+
     return products;
+  } catch (error) {
+    console.error("Error loading products.", error);
+  }
 }
